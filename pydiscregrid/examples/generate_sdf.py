32fa5ef52b6242865eb7fe6b8bcb1a49086c8ee0
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2020-04-20 19:57:58 +0200

    Python bindings for all the things.

---
+++ pydiscregrid/examples/generate_sdf.py
@@ -0,0 +1,32 @@
+import pydiscregrid
+import numpy as np
+import meshio
+
+def main():
+    print("Loading mesh...")
+    mesh = meshio.read("C:/Users/Stefan/Downloads/treefrog_45_cut.stl")
+    tri_mesh = pydiscregrid.TriangleMesh(mesh.points, mesh.cells[0].data)
+    print(f"\tFound {tri_mesh.nVertices()} vertices and {tri_mesh.nFaces()} faces.")
+    print("\tDone.")
+
+    print("Set up data structures...")
+    md = pydiscregrid.MeshDistance(tri_mesh)
+    print("\tDone.")
+
+    print("Computing bounding box...")
+    max = np.max(mesh.points, axis=0)
+    min = np.min(mesh.points, axis=0)
+    print(f"\tMin {min}\n\tMax {max}")
+    min -= 1e-3 * np.linalg.norm(max - min, ord=2) * np.ones_like(min)
+    max += 1e-3 * np.linalg.norm(max - min, ord=2) * np.ones_like(max)
+    print(f"\tMin {min}\n\tMax {max}")
+    grid = pydiscregrid.CLDiscreteGrid(min, max, [20, 20, 20])
+    print(f"\tConstructed {grid.nCells()} cells.")
+
+    print(f"Generate Discretization...")
+
+    grid.addSDF(md, True)
+    print(f"\tDone.")
+
+if __name__ == '__main__':
+    main()
---
+++ pydiscregrid/main.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Stefan on 4/20/2020.
+//
+
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <pybind11/functional.h>
+#include <pybind11/stl.h>
+
+#include <Discregrid/All>
+
+namespace py = pybind11;
+using namespace pybind11::literals;
+
+PYBIND11_MODULE(pydiscregrid, m){
+
+    py::class_<Discregrid::CubicLagrangeDiscreteGrid>(m, "CLDiscreteGrid")
+            .def(py::init<const std::string>())
+            .def(py::init<const Eigen::AlignedBox3d, std::array<unsigned int, 3>>())
+            .def(py::init<>([](Eigen::Vector3d const& min, Eigen::Vector3d const& max, std::array<unsigned int, 3>const& res){
+                Eigen::AlignedBox3d box(min, max);
+                return Discregrid::CubicLagrangeDiscreteGrid(box, std::forward<std::array<unsigned int, 3>const&>(res));
+            }))
+            .def("save", &Discregrid::CubicLagrangeDiscreteGrid::save)
+            .def("load", &Discregrid::CubicLagrangeDiscreteGrid::load)
+            .def("addFunction", &Discregrid::CubicLagrangeDiscreteGrid::addFunction, "func"_a, "verbose"_a=false, py::arg_v("predicate", (Discregrid::DiscreteGrid::SamplePredicate*)nullptr, "Sample Predicate std::function<bool(Eigen::Vector3d const&)>"))
+            .def("addSDF", [](Discregrid::CubicLagrangeDiscreteGrid & obj, const Discregrid::MeshDistance & md, bool verbose){
+                auto func = Discregrid::DiscreteGrid::ContinuousFunction {};
+                func = [&md](Eigen::Vector3d const& x){ return md.signedDistanceCached(x); };
+                obj.addFunction(func, verbose);
+            })
+            .def("nCells", &Discregrid::CubicLagrangeDiscreteGrid::nCells)
+            .def("determineShapeFunctions", &Discregrid::CubicLagrangeDiscreteGrid::determineShapeFunctions, "field_id"_a, "x"_a, "cell"_a, "c0"_a, "N"_a, "dN"_a=nullptr)
+            .def("interpolate", (double (Discregrid::CubicLagrangeDiscreteGrid::*) (unsigned int, Eigen::Vector3d const &, const std::array<unsigned int, 32> &, const Eigen::Vector3d &, const Eigen::Matrix<double, 32, 1> &, Eigen::Vector3d *, Eigen::Matrix<double, 32, 3> *dN)const)(&Discregrid::CubicLagrangeDiscreteGrid::interpolate),
+                    "field_id"_a, "xi"_a, "cell"_a, "c0"_a, "N"_a, "gradient"_a= nullptr, "dN"_a= nullptr)
+            .def("interpolate", (double (Discregrid::CubicLagrangeDiscreteGrid::*)(unsigned int, Eigen::Vector3d const &, Eigen::Vector3d *)const)(&Discregrid::CubicLagrangeDiscreteGrid::interpolate),
+                 "field_id"_a, "xi"_a, "gradient"_a=nullptr);
+
+    py::class_<Discregrid::TriangleMesh>(m, "TriangleMesh")
+            .def(py::init<std::vector<Eigen::Vector3d>const&, std::vector<std::array<unsigned int, 3>> const&>())
+            .def(py::init<std::string const&>())
+            .def("nFaces", &Discregrid::TriangleMesh::nFaces)
+            .def("nVertices", &Discregrid::TriangleMesh::nVertices);
+
+    py::class_<Discregrid::MeshDistance>(m, "MeshDistance")
+            .def(py::init<Discregrid::TriangleMesh const&, bool>(), "mesh"_a, "precompute_normals"_a=true)
+            .def("distance", &Discregrid::MeshDistance::distance, "x"_a, "nearestPoint"_a= nullptr, "nearestFace"_a= nullptr, "nearestEntity"_a= nullptr)
+            .def("signedDistance", &Discregrid::MeshDistance::signedDistance, "x"_a)
+            .def("signedDistanceCached", &Discregrid::MeshDistance::signedDistance, "x"_a);
+}
